5b2497bc65af19844a7b30303ef90c4511a5c285
blck-snwmn <whitesnowmancreator@gmail.com>
2019-08-14 13:20:30 +0900

    Change to use mutex at getRoom

--- chatroot.go
+++ chatroot.go
@@ -1,13 +1,21 @@
 package main
 
-import "github.com/gorilla/websocket"
+import (
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
 
 // ChatRoot is Chat's root
 type ChatRoot struct {
+	mu    sync.Mutex
 	rooms map[string]*Room
 }
 
 func (root *ChatRoot) getRoom(name string) *Room {
+	root.mu.Lock()
+	defer root.mu.Unlock()
+
 	room, exist := root.rooms[name]
 	if !exist {
 		room = &Room{
@@ -17,4 +25,8 @@ func (root *ChatRoot) getRoom(name string) *Room {
 		root.rooms[name] = room
 	}
 	return room
+}
+
+func newChatRoom() *ChatRoot {
+	return &ChatRoot{rooms: make(map[string]*Room)}
 }
